357997498bfd1b50ee4a68468a22c291c77b3480
TheUltimateOptimist <digitalConfidence@web.de>
2021-09-06 11:58:36 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/baustoffhandel_dueck/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.digital_confidence.baustoffhandel_dueck
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final RoundedLoadingButtonController roundedLoadingButtonController =
+      RoundedLoadingButtonController();
+  @override
+  void initState() {
+    launchWebsite("http://baustoffhandel-dueck.de.dedi3087.your-server.de");
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      roundedLoadingButtonController.start();
+    });
+    super.initState();
+  }
+
+  Future<void> launchWebsite(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    } else
+      throw "Counld not launch $url";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: RoundedLoadingButton(
+        onPressed: () {},
+        controller: roundedLoadingButtonController,
+        child: Text(
+          "",
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 30,
+          ),
+        ),
+      )),
+    );
+  }
+}
